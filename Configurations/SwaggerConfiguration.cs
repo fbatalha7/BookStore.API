9e8d14239b58f9594e3bae8381b515f1a85036a5
Felipe B <felipebatalhanasc@gmail.com>
2025-07-19 06:57:38 -0300

    feat: adicionando documentação swagger

---
+++ Configurations/SwaggerConfiguration.cs
@@ -0,0 +1,24 @@
+﻿namespace BookStore.API.Configurations
+{
+    public static class SwaggerConfiguration
+    {
+        public static void AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "BookStore API",
+                    Version = "v1",
+                    Description = "API for managing a bookstore",
+                    Contact = new()
+                    {
+                        Name = "BookStore Support",
+                        Email = "example@email.com"
+                    }
+                });
+            });
+        }
+
+    }
+}
