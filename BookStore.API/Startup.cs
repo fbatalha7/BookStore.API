67eae9a3fd8089d2fce76d3644253ad3cadc4ef8
Felipe B <felipebatalhanasc@gmail.com>
2025-07-19 20:35:06 -0300

    refactor: ajustando direcionamento dos arquivos de build

---
+++ BookStore.API/Startup.cs
@@ -0,0 +1,48 @@
+﻿using BookStore.API.Configurations;
+using BookStore.Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.API
+{
+    public class Startup(IConfiguration configuration)
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddEndpointsApiExplorer();
+
+            services.AddSwaggerConfiguration();
+
+            services.DependecyInjections();
+
+            services.AddAutoMapperConfiguration();
+
+            AddInfraConfiguration(services);
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.AddMigrations();
+
+            app.UseDeveloperExceptionPage();
+
+            app.UseSwaggerConfiguration();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+        private void AddInfraConfiguration(IServiceCollection services)
+            => services.AddDbContext<AppDbContext>(options
+                => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+    }
+}
