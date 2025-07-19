7fc41f533b1c8b85988bec2db6f43b93ba153ac3
Felipe B <felipebatalhanasc@gmail.com>
2025-07-19 07:36:18 -0300

    feat: aplicando conceitos de arquitetura limpa e DDD

---
+++ Controllers/BookController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController : ControllerBase
+    {
+        [HttpGet()] 
+        public ActionResult Get() =>
+            Ok(new { Message = "Welcome to the BookStore API!" });
+    }
+}
