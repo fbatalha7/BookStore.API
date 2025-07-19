0c9560e029ea1926f744f27fb1cb8f28968558a7
Felipe B <felipebatalhanasc@gmail.com>
2025-07-19 07:20:23 -0300

    refactor: criando extensoes de configurações

--- Program.cs
+++ Program.cs
@@ -1,40 +1,14 @@
-using BookStore.API.Configurations;
-
 namespace BookStore.API
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
 
-            builder.Services.AddControllers();
-            builder.Services.AddOpenApi();
-
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerConfiguration();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-                app.MapOpenApi();
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.UseSwagger();
-
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "BookStore API V1");
-                c.RoutePrefix = "api"; 
-            });
-
-            app.Run();
-        }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
